286f0360ade74548fbf272848b65bef5f0993d8b
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-05-08 23:18:55 +0545

    axios hook

---
+++ src/hooks/useAxios.tsx
@@ -0,0 +1,15 @@
+import axios, { AxiosRequestConfig } from "axios"
+
+interface AxiosConfig extends AxiosRequestConfig {
+    
+}
+
+const useAxios = () => {
+    axios.config({
+        url: 
+    })
+  return (
+    <div>useAxios</div>
+  )
+}
+export default useAxios
