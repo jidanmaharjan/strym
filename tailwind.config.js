e6d1e224374ea333b5b2042753d85c6de0d4140b
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-05-08 22:07:36 +0545

    changed brand name

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6c40aa",
+        background: "#f3f3f3",
+        light: "#7c5daf",
+        fade: "#8d7ab4",
+        grey: "#9e97b9",
+      },
+    },
   },
   plugins: [],
 };
