03c3084261b625b61cc6c71c7716db9cb79fe814
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-05-13 23:27:07 +0545

    featured sample

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,4 +1,6 @@
-import { Carousel } from "antd";
+import { Button, Carousel } from "antd";
+import { IoHeartCircleOutline } from "react-icons/io5";
+import { getRandomColorPair } from "../constants/helpers";
 
 const Featured = () => {
   const contentStyle: React.CSSProperties = {
@@ -7,10 +9,33 @@ const Featured = () => {
     textAlign: "center",
     background: "#364d79",
   };
+  const randomColor = getRandomColorPair();
   return (
-    <Carousel autoplay className="mb-4 h-48">
+    <Carousel className="mb-4 h-48 rounded-lg">
       <div>
-        <h3 style={contentStyle}>1</h3>
+        <div
+          style={{ background: randomColor.secondaryColor }}
+          className="w-full h-48 flex justify-between rounded-lg"
+        >
+          <div>
+            <p>Personally Selected</p>
+            <h2>Title</h2>
+            <h3>Artist</h3>
+            <div>
+              <Button>Listen Now</Button>
+              <Button>
+                <IoHeartCircleOutline />
+              </Button>
+            </div>
+          </div>
+          <div>
+            <img
+              src="https://source.unsplash.com/random/800x600"
+              alt="cover"
+              className="h-full w-full object-cover rounded-lg"
+            />
+          </div>
+        </div>
       </div>
       <div>
         <h3 style={contentStyle}>2</h3>
