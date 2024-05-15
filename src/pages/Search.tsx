307670773219ddf6659b257f1088bc39ebe12b02
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-05-15 23:08:20 +0545

    filter of options

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,4 +1,4 @@
-import { Input } from "antd";
+import { Button, Checkbox, Input, Modal } from "antd";
 import { useEffect, useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { useQuery } from "react-query";
@@ -8,19 +8,34 @@ import ArtistCards from "./components/ArtistCards";
 import TrackCards from "./components/TrackCards";
 import AlbumCards from "./components/AlbumCards";
 import PlaylistCards from "./components/PlaylistCards";
-import { TbDatabaseSearch } from "react-icons/tb";
+import { TbDatabaseSearch, TbFilter } from "react-icons/tb";
+
+const filterOptions = [
+  { label: "Track", value: "track" },
+  { label: "Artist", value: "artist" },
+  { label: "Album", value: "album" },
+  { label: "Playlist", value: "playlist" },
+];
 
 const Search = () => {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
   const [query, setQuery] = useState(searchParams.get("q") || "");
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [filters, setFilters] = useState([
+    "track",
+    "artist",
+    "album",
+    "playlist",
+  ]);
+
   const { data, isLoading, isFetching, refetch } = useQuery(
     ["searchSpotify"],
     () =>
       searchSpotfy({
         q: query,
-        type: "track,artist,album,playlist",
+        type: filters.join(","),
         limit: 10,
         include_external: "audio",
       }),
@@ -39,12 +54,12 @@ const Search = () => {
       }
     }, 700);
     return () => clearTimeout(timer);
-  }, [query]);
+  }, [query, filters]);
 
   return (
     <div className="p-4">
       <form
-        className="flex-grow flex justify-center sticky top-20 z-40"
+        className="flex-grow flex justify-center items-center gap-4 sticky top-20 z-40"
         onSubmit={(e) => {
           e.preventDefault();
         }}
@@ -56,25 +71,52 @@ const Search = () => {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
+        <Button
+          type="primary"
+          shape="circle"
+          // size="large"
+          onClick={() => setIsModalOpen(true)}
+          icon={<TbFilter size={20} />}
+        ></Button>
+        <Modal
+          title="Filter"
+          open={isModalOpen}
+          onCancel={() => setIsModalOpen(false)}
+          footer={null}
+        >
+          <Checkbox.Group
+            options={filterOptions}
+            defaultValue={filters}
+            onChange={(values) => setFilters(values as string[])}
+          />
+        </Modal>
       </form>
       {query ? (
         <div className="mt-4 grid gap-2">
-          <ArtistCards
-            data={data?.artists?.items}
-            loading={isLoading || isFetching}
-          />
-          <AlbumCards
-            data={data?.albums?.items}
-            loading={isLoading || isFetching}
-          />
-          <TrackCards
-            data={data?.tracks?.items}
-            loading={isLoading || isFetching}
-          />
-          <PlaylistCards
-            data={data?.playlists?.items}
-            loading={isLoading || isFetching}
-          />
+          {filters.includes("artist") && (
+            <ArtistCards
+              data={data?.artists?.items}
+              loading={isLoading || isFetching}
+            />
+          )}
+          {filters.includes("album") && (
+            <AlbumCards
+              data={data?.albums?.items}
+              loading={isLoading || isFetching}
+            />
+          )}
+          {filters.includes("track") && (
+            <TrackCards
+              data={data?.tracks?.items}
+              loading={isLoading || isFetching}
+            />
+          )}
+          {filters.includes("playlist") && (
+            <PlaylistCards
+              data={data?.playlists?.items}
+              loading={isLoading || isFetching}
+            />
+          )}
         </div>
       ) : (
         <div className="flex flex-col justify-center items-center mt-4 p-8 w-full h-80 gap-4 text-grey">
