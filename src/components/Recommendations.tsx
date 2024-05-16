e39b698e17985285e31503490406c2cf3a655206
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-05-16 23:15:47 +0545

    recommendation improved

--- src/components/Recommendations.tsx
+++ src/components/Recommendations.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import { callAxios } from "../hooks/useAxios";
 import Featured from "./Featured";
+import { random } from "lodash";
 
 const defaultSeedArtists = ["41MozSoPIsD1dJM0CLPjZF"];
 const defaultSeedGenres = ["k-pop"];
@@ -17,18 +18,43 @@ const Recommendations = () => {
       url: "recommendations",
       params: {
         limit: 10,
-        seed_artists:
-          storedSeedArtists.length > 0
-            ? storedSeedArtists?.slice(3).join(",")
-            : defaultSeedArtists.join(","),
-        seed_genres:
-          storedSeedGenres.length > 0
-            ? storedSeedGenres?.slice(2).join(",")
-            : defaultSeedGenres.join(","),
+        seed_artists: [
+          ...defaultSeedArtists,
+          ...storedSeedArtists.slice(
+            random(
+              0,
+              storedSeedArtists.length > 2 ? storedSeedArtists.length - 2 : 0
+            ),
+            2
+          ),
+        ].join(","),
+        seed_genres: [
+          ...defaultSeedGenres,
+          ...storedSeedGenres.slice(
+            random(
+              0,
+              storedSeedGenres.length > 1 ? storedSeedGenres.length - 1 : 0
+            ),
+            1
+          ),
+        ].join(","),
         include_external: "audio",
       },
     })
   );
+  console.log(
+    [
+      ...defaultSeedArtists,
+      ...storedSeedArtists.slice(
+        random(
+          0,
+          storedSeedArtists.length > 2 ? storedSeedArtists.length - 2 : 0
+        ),
+        2
+      ),
+    ].join(",")
+  );
+
   return <Featured data={data?.tracks} loading={isFetching} />;
 };
 
